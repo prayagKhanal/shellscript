622b409b9204084bea13a5434e3a9083bbcfbb35
Prayag <prayag.khanal@gmail.com>
2017-03-23 18:49:47 -0700

    2nd progression

--- clearlog.bash
+++ clearlog.bash
@@ -1,7 +1,27 @@
+#!/bin/bash
+#2nd progression to shellscript
 
-# Cleanup
-# Run as root, of course.
-cd /var/log
-cat /dev/null > messages
-cat /dev/null > wtmp
-echo "Log files cleaned up."
+# Cleanup, version 2
+
+# User as root prayagKhanal
+# Look at your log file if there is any error
+
+LOG_DIR = /var/log
+
+# using variable to store path
+
+cd $LOG_DIR
+ 
+ # im in my var/log
+ 
+ cat /dev/null > message
+ cat /dev/null > wtmp
+ 
+ #cleaning my log file
+ 
+ echo "Logs cleaned up."
+ 
+ exit
+ 
+ # If permission denied error occurs run it with sudo command
+ 
